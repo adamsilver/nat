4571dc80db75af6019ce38bd6e02d2318c0a1369
Adam Silver <adamsilver@Adams-MBP-2025.lan>
2025-05-02 15:31:18 +0100

    Create prototype

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,8 @@
+//
+// For guidance on how to add JavaScript see:
+// https://docs.nowprototype.it/0.11.4/in-browser-javascript
+//
+
+window.nowPrototypeItGOVUK.documentReady(() => {
+  // Add JavaScript here
+})
---
+++ app/routes.js
@@ -0,0 +1,9 @@
+//
+// For guidance on how to create routes see:
+// https://docs.nowprototype.it/0.11.4/routers/create-routes
+//
+
+const prototypeCore = require('nowprototypeit')
+const router = prototypeCore.requests.setupRouter()
+
+// Add your routes here
